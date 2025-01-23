371e6366b29446b9ac15e885517d80ea0579ce99
MeetPrajapati <meet18052005@gmail.com>
2025-01-23 08:46:30 -0500

    Project is ready to deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy:{
-      '/api/v1' : 'http://localhost:8000',
+      '/api/v1' : 'https://you-tube-orcin-eight.vercel.app/',
     },
   },
   plugins: [react()],
